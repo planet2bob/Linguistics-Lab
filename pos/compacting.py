11dd26172056fdcf892d539a7dc2db0d8a20c6d8
Christian Wu <jchristian99@gmail.com>
2016-07-05 19:51:14 -0400

    array < dictionary

--- pos/compacting.py
+++ pos/compacting.py
@@ -1,26 +1,16 @@
 import csv, sys
 
 # filename = sys.argv[1]
-row_number, column_number = 1, 1
-patterns = [['',''],['','']]
+
+patterns = {}
 
 with open("testing.csv", 'rb') as vb:
     rows = list(csv.reader(vb, lineterminator = '\n', dialect='excel'))
     #print rows[row_number][column_number]
+    for i in range(len(rows)):
+    	try:
+    		patterns[rows[i][0]] = patterns[rows[i][0]] + 1
+    	except:
+    		patterns[rows[i][0]] = 1
 
-with open("testing.csv", 'rb') as vb:
-	rows = list(csv.reader(vb, lineterminator = '\n', dialect='excel'))
-	for i in range(len(rows)):
-		pattern = ""
-		for l in range(len(rows[i])):
-			pattern += (str(rows[i][l]))
-		placement = -1
-		for l in range(2):
-			if(patterns[l][0] == pattern):
-				placement = l
-		if placement == -1:
-			patterns.append(pattern)
-		else:
-			patterns[l][0] += 1
-
-		print patterns[:]
+print patterns
